80f81274a75c9d9abb74f963dea5662a6c0e5202
Matcry12 <fdtywadw@gmail.com>
2021-06-12 23:43:10 +0700

    bluezone loi

---
+++ app/src/main/java/com/szxc/bluezone/Thesecret.kt
@@ -0,0 +1,19 @@
+package com.szxc.bluezone
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Thesecret : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thesecret)
+        val button = findViewById<Button>(R.id.back)
+        button.setOnClickListener() {
+            val intent: Intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
