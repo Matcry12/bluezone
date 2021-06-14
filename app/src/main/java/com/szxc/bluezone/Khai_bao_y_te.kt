12cf98442961a9de22464b7a181b6b6de441157f
Matcry12 <fdtywadw@gmail.com>
2021-06-14 09:54:10 +0700

    cập nhật

--- app/src/main/java/com/szxc/bluezone/Khai_bao_y_te.kt
+++ app/src/main/java/com/szxc/bluezone/Khai_bao_y_te.kt
@@ -13,43 +13,30 @@ class Khai_bao_y_te : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(
             this, R.layout.activity_main)
-        val cuakhau = binding.khaiBaoCuaKhau
 
-        val name = binding.hoten
 
-        val date = binding.namsinh
-
-        val gioitinh = binding.gioitinh
-
-        val quoctinh = binding.quoctinh
-
-        val sohochieuGiaythonghanh = binding.sohochieuGiaythonghanh
-
-        val tiepxuc = binding.tiepxuc
-
-        val button = binding.khaiBao
-
-        button.setOnClickListener() {
+        binding.khaiBao.setOnClickListener() {
             val intent : Intent = Intent(this, displaythefillinformation::class.java)
-            val dulieunhap1 = cuakhau.text.toString()
+
+            val dulieunhap1 = binding.khaiBaoCuaKhau.text.toString()
             intent.putExtra("dulieu1", dulieunhap1)
 
-            val dulieunhap2 = name.text.toString()
+            val dulieunhap2 = binding.hoten.text.toString()
             intent.putExtra("dulieu2", dulieunhap2)
 
-            val dulieunhap3 = date.text.toString()
+            val dulieunhap3 = binding.namsinh.text.toString()
             intent.putExtra("dulieu3", dulieunhap3)
 
-            val dulieunhap4 = gioitinh.text.toString()
+            val dulieunhap4 = binding.gioitinh.text.toString()
             intent.putExtra("dulieu4", dulieunhap4)
 
-            val dulieunhap5 = quoctinh.text.toString()
+            val dulieunhap5 = binding.quoctinh.text.toString()
             intent.putExtra("dulieu5", dulieunhap5)
 
-            val dulieunhap6 = sohochieuGiaythonghanh.text.toString()
+            val dulieunhap6 = binding.sohochieuGiaythonghanh.text.toString()
             intent.putExtra("dulieu6", dulieunhap6)
 
-            val dulieunhap7= tiepxuc.text.toString()
+            val dulieunhap7= binding.tiepxuc.text.toString()
             intent.putExtra("dulieu7", dulieunhap7)
 
 
--- app/src/main/java/com/szxc/bluezone/displaythefillinformation.kt
+++ app/src/main/java/com/szxc/bluezone/displaythefillinformation.kt
@@ -5,12 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.szxc.bluezone.databinding.ActivityDisplaythefillinformationBinding
+import com.szxc.bluezone.databinding.ActivityMainBinding
 
 class displaythefillinformation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_displaythefillinformation)
+        val binding : ActivityDisplaythefillinformationBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_displaythefillinformation)
+
         val bundle: Bundle? = intent.extras
+
         val dulieunhan1 = bundle!!.getString("dulieu1")
 
         val dulieunhan2 = bundle.getString("dulieu2")
@@ -25,45 +31,32 @@ class displaythefillinformation : AppCompatActivity() {
 
         val dulieunhan7 = bundle.getString("dulieu7")
 
-        var lichtrinh = findViewById<TextView>(R.id.lichtrinh1)
 
-        var hoten = findViewById<TextView>(R.id.hoten1)
+        binding.lichtrinh1.text = dulieunhan1
 
-        var namsinh = findViewById<TextView>(R.id.namsinh1)
+        binding.hoten1.text = dulieunhan2
 
-        var gioitinh = findViewById<TextView>(R.id.gioitinh1)
+        binding.namsinh1.text = dulieunhan3
 
-        var quoctich = findViewById<TextView>(R.id.quoctinh1)
+        binding.gioitinh1.text = dulieunhan4
 
-        var sohochieugiaythonghanh = findViewById<TextView>(R.id.sohochieu_giaythonghanh1)
+        binding.quoctich1.text = dulieunhan5
 
-        var tiepxuc = findViewById<TextView>(R.id.tiepxuc1)
+        binding.sohochieuGiaythonghanh1.text = dulieunhan6
 
-        lichtrinh.text = dulieunhan1
-
-        hoten.text = dulieunhan2
-
-        namsinh.text = dulieunhan3
-
-        gioitinh.text = dulieunhan4
-
-        quoctich.text = dulieunhan5
-
-        sohochieugiaythonghanh.text = dulieunhan6
-
-        tiepxuc.text = dulieunhan7
+        binding.tiepxuc1.text = dulieunhan7
 
 
 
-                var go = findViewById<Button>(R.id.gotothesecret)
 
-                go.setOnClickListener() {
+
+                binding.gotothesecret.setOnClickListener() {
                     val intent: Intent = Intent(this, Thesecret::class.java)
                     startActivity(intent)
                 }
 
-                val back = findViewById<Button>(R.id.fastback)
-                back.setOnClickListener() {
+
+                binding.fastback.setOnClickListener() {
                     val intent: Intent = Intent(this, Khai_bao_y_te::class.java)
                     startActivity(intent)
                     finish()
